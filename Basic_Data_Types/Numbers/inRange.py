4a9734452fbed9c0781eed27633d262ace95cf40
zowa <elfilfatimaezzahra0@gmail.com>
2024-10-14 02:28:40 +0100

    Find Values Within a Range

---
+++ Basic_Data_Types/Numbers/inRange.py
@@ -0,0 +1,11 @@
+def inRange(x, y):
+	# Comparisons can be chained
+	return (x < 1/3 <y)
+
+	# if (x < 1 / 3) and (y > 1 / 3):
+	# 	return True
+	# else:
+	# 	return False
+
+print(inRange(1, 2))
+print(inRange(0, 2))
