b0a8341c258401e2a2d762164c7836f0a9748765
zowa <elfilfatimaezzahra0@gmail.com>
2024-10-15 17:50:09 +0100

    Implement Getter Methods

--- Classes/Classes/Rectangle.py
+++ Classes/Classes/Rectangle.py
@@ -1,5 +1,5 @@
 class Rectangle:
-	def __init__(self, x1, x2, y1, y2):
+	def __init__(self, x1, y1, x2, y2):
 		if x1 < x2 and y1 > y2:
 			self.x1 = x1
 			self.x2 = x2
@@ -8,6 +8,15 @@ class Rectangle:
 			print('Rectangle created')
 		else:
 			print('You cannot create a rectangle with these coord')
+	
+	def width(self):
+		return self.x2 - self.x1
+	
+	def height(self):
+		return self.y1 - self.y2
 
 
-rec = Rectangle(2,3,5,1)
+rec = Rectangle(2,7,8,4)
+h = rec.height()
+w = rec.width()
+print(f'height = {h}, wigth = {w}')
