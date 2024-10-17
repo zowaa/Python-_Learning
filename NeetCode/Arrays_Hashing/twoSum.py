c35abce64e988cfa294c8a04e723e220510d3222
zowa <elfilfatimaezzahra0@gmail.com>
2024-10-17 19:18:29 +0100

    Two Integer Sum

---
+++ NeetCode/Arrays_Hashing/twoSum.py
@@ -0,0 +1,17 @@
+class Solution:
+	# def twoSum(self,nums,target):
+	# 	for i in range(len(nums)-1):
+	# 		for j in range(i+1, len(nums)):
+	# 			if nums[i] + nums[j] == target:
+	# 				return [i,j]
+
+	def twoSum(self,nums,target):
+		hash_map = {}
+		for n in range(len(nums)):
+			rem = target - nums[n]
+			if rem in hash_map:
+				return [hash_map[rem],n]
+			hash_map[nums[n]] = n
+
+				
+print(Solution().twoSum([4,5,6],10))
