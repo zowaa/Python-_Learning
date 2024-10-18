c73f23ffe00760447ba088aae45208f0259995dc
zowa <elfilfatimaezzahra0@gmail.com>
2024-10-18 15:01:50 +0100

    Use of defaultdict

--- NeetCode/Arrays_Hashing/groupAnagram.py
+++ NeetCode/Arrays_Hashing/groupAnagram.py
@@ -1,14 +1,16 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs):
-        mapp = {}
+        mapp = defaultdict(list)
         for word in strs:
             sorted_word = ''.join(sorted(word))
-            if sorted_word in mapp:
-                mapp[sorted_word].append(word)
-            else:
-                mapp[sorted_word] = [word]
+            mapp[sorted_word].append(word)
         return list(mapp.values())
 
 f = ["act","pots","tops","cat","stop","hat"]
 
 print(Solution().groupAnagrams(f))
+
+# Time Complexity: O(k * n log n)
+# Space Complexity: O(n * k)
